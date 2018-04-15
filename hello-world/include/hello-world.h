802468add6dd537748b83ab209a8b0f7a8b402c1
garethwa <gareth.warwick@outlook.com>
2018-04-15 11:31:43 +0100

    Setting up c env with hello-world example

---
+++ hello-world/include/hello-world.h
@@ -0,0 +1,5 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+#endif/*HELLO_WORLD_H*/
+
+void say_hello();
---
+++ hello-world/src/hello-world.c
@@ -0,0 +1,12 @@
+#include "hello-world.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	say_hello();
+}
+
+void say_hello()
+{
+	printf("Hello, world!\n");
+}
