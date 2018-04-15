b77624fcfa1a201826d38d4bfb9ae0d7affd49c2
garethwa <gareth.warwick@outlook.com>
2018-04-15 12:00:33 +0100

    Re-format hello-world.c functions and have make create binaries in bin

--- hello-world/src/hello-world.c
+++ hello-world/src/hello-world.c
@@ -1,12 +1,14 @@
 #include "hello-world.h"
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+int 
+main(int argc, char* argv[])
 {
 	say_hello();
 }
 
-void say_hello()
+void 
+say_hello()
 {
 	printf("Hello, world!\n");
 }
